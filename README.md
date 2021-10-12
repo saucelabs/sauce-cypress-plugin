b8b7b391e40020aa807787fbbe623798049c497e
Félix P <felix@saucelabs.com>
2021-10-12 11:33:51 -0700

    [DEVX-1245] Add pipeline for testing plugin against Cypress (#3)

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # sauce-cypress-plugin
 
-This Cypress plugins reports each spec to your Sauce Labs account. When you run tests with the Cypress CLI, using this plugin, you can send the test results to [Sauce Labs](https://app.saucelabs.com).
+This Cypress plugins reports each spec to your Sauce Labs account.
+
+When you run tests with the Cypress CLI, using this plugin, test results and artifacts are uploaded to [Sauce Labs](https://app.saucelabs.com).
 
 ## Installation
 
@@ -52,4 +54,6 @@ Example:
 | tags | Sets tags | Array of String |
 | region | Sets the region (Default: `us-west-1`) | String |
 
+## Real-life example
 
+[tests/integration/](https://github.com/saucelabs/sauce-cypress-plugin/tree/main/tests/integration/) folder will present an integration example with [Cypress' Kitchensink](https://github.com/cypress-io/cypress-example-kitchensink/tree/master/cypress/integration/2-advanced-examples) tests set.
---
+++ tests/integration/cypress.spec.js
@@ -0,0 +1,85 @@
+require('jest');
+
+const {exec} = require('child_process');
+const {readFile} = require('fs/promises');
+const axios = require('axios');
+const path = require('path');
+
+const jobUrlPattern = /https:\/\/app\.saucelabs\.com\/tests\/([0-9a-f]{32})/g
+const specFile = 'examples/actions.spec.js';
+
+let hasError;
+let output;
+
+describe('runs tests on cloud', function () {
+  beforeAll(async function () {
+    const cypressRunCommand = `./node_modules/.bin/cypress run --spec cypress/integration/${specFile}`;
+    const execOpts = {
+      cwd: __dirname,
+      env: process.env,
+    };
+
+    const p = new Promise((resolve) => {
+      exec(cypressRunCommand, execOpts, async function (err, stdout) {
+        hasError = err;
+        output = stdout;
+        resolve();
+      });
+    });
+    await p;
+  });
+
+  test('cypress execution passed', function () {
+    expect(hasError).toBeNull();
+  });
+
+  test('jobs link is displayed', function () {
+    const jobs = {};
+    const jobIDs = output.match(jobUrlPattern);
+
+    for (const job of jobIDs) {
+      const idx = job.slice(job.lastIndexOf('/')+1);
+      jobs[idx] = (jobs[idx] || 0) + 1
+    }
+    expect(Object.keys(jobs).length).toBe(1);
+    for (const idx of Object.keys(jobs)) {
+      expect(jobs[idx]).toBe(2);
+    }
+  });
+
+  test('job has video.mp4/console.log attached', async function () {
+    let jobId = output.match(jobUrlPattern)[0];
+    jobId = jobId.slice(jobId.lastIndexOf('/')+1);
+
+    const url = `https://api.us-west-1.saucelabs.com/rest/v1/jobs/${jobId}/assets`;
+    const response = await axios.get(url, {
+      auth: {
+        username: process.env.SAUCE_USERNAME,
+        password: process.env.SAUCE_ACCESS_KEY,
+      }
+    });
+    const assets = response.data;
+    expect(assets['console.log']).toBe('console.log');
+    expect(assets['video.mp4']).toBe('video.mp4');
+    expect(assets.video).toBe('video.mp4');
+  });
+
+  test('job has name/tags correctly set', async function () {
+    const cypressConfig = JSON.parse(await readFile(path.join(__dirname, 'cypress.json')));
+
+    let jobId = output.match(jobUrlPattern)[0];
+    jobId = jobId.slice(jobId.lastIndexOf('/')+1);
+
+    const url = `https://api.us-west-1.saucelabs.com/rest/v1/jobs/${jobId}`;
+    const response = await axios.get(url, {
+      auth: {
+        username: process.env.SAUCE_USERNAME,
+        password: process.env.SAUCE_ACCESS_KEY,
+      }
+    });
+    const jobDetails = response.data;
+    expect(jobDetails.passed).toBe(true);
+    expect(jobDetails.tags.sort()).toEqual(cypressConfig.sauce.tags.sort());
+    expect(jobDetails.name).toBe(`${cypressConfig.sauce.build} - ${specFile}`);
+  });
+});
