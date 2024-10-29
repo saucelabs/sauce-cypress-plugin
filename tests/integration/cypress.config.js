91d7b07732e7291f30da67be486b6672d42a7c1a
Tian Feng <tian.feng@saucelabs.com>
2024-10-29 16:25:52 -0700

    revert integration setup for fs operation

--- tests/integration/cypress.config.js
+++ tests/integration/cypress.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require("cypress");
-const fs = require("fs");
-const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
@@ -10,26 +8,6 @@ module.exports = defineConfig({
         build: "Cypress Kitchensink Example",
         tags: ["plugin", "kitchensink", "cypress"],
         region: "us-west-1",
-      });
-
-      on("task", {
-        readFileAsStream({ filePath }) {
-          return new Promise((resolve, reject) => {
-            const resolvedPath = path.resolve(filePath);
-            if (!fs.existsSync(resolvedPath)) {
-              return reject(
-                new Error(`File not found at path: ${resolvedPath}`),
-              );
-            }
-
-            const stream = fs.createReadStream(resolvedPath);
-            const chunks = [];
-
-            stream.on("data", (chunk) => chunks.push(chunk));
-            stream.on("end", () => resolve(Buffer.concat(chunks).toString()));
-            stream.on("error", reject);
-          });
-        },
       });
 
       return config;
